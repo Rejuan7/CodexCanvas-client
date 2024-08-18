5e1cb8b60624f28dc193c2b179cb8b6eb7dc9e84
Rejuan <rejuanjr2000@gmail.com>
2024-08-18 18:14:49 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl font-bold ">Hello world!</h1>
+      <button className="btn btn-active btn-accent">Link</button>
+      <h1 className="text-4xl"></h1>
+    </>
+  );
+}
+
+export default App;
